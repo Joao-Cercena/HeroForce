ac2639c9139c01847c30b53c5a77b0d9a7742fb1
joao.cercena <joaov.cercena@gmail.com>
2025-07-05 19:19:58 -0300

    Estrutura inicial do backend

---
+++ heroforce-backend/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './typeorm.config';
+import { AuthModule } from './auth/auth.module';
+import { ProjectsModule } from './projects/projects.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    AuthModule,
+    ProjectsModule,
+    UsersModule,
+  ],
+})
+export class AppModule {}
---
+++ heroforce-backend/src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { UsersService } from './users/users.service';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+    const usersService = app.get(UsersService);
+  await seedAdminUser(usersService);
+  await app.listen(process.env.PORT ?? 3001);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
+
+
+async function seedAdminUser(usersService: UsersService) {
+  const email = 'stan.lee@heroforce.com';
+  const existing = await usersService.findOneByEmail(email);
+
+  if (!existing) {
+    await usersService.create({
+      name: 'Stan Lee',
+      email,
+      password: 'Excelsior#1962',
+      heroCharacter: 'The Watcher',
+      isAdmin: true,
+    } as any);
+    console.log('👤 Usuário admin "Stan Lee" criado!');
+  }
+}
